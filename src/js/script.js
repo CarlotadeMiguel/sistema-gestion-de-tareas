81672b65e457903b26578010ee84d13abf4d7fad
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-03-17 19:45:45 +0100

    feat: Mostar detalles de la tarea y ocultarlos

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded');
-});
-document.addEventListener('DOMContentLoaded', () => {
 
     /* Gestor de Tareas */
+    const contenedorTareas = document.getElementById('tareas');
+    let dialog;
 
     // Función para cargar datos desde localStorage
     const cargarDatos = () => {
         return JSON.parse(localStorage.getItem('tareas')) || [];
     }
+    const tareas = cargarDatos();
 
     // Función para guardar datos en localStorage
     const guardarDatos = (tareas) => {
@@ -17,27 +17,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para agregar una nueva tarea
     const agregarTarea = (tarea) => {
-        const tareas = cargarDatos();
         tareas.push(tarea);
         guardarDatos(tareas);
     }
 
     // Función para mostrar las tareas guardadas
     const mostrarTareas = () => {
-        const tareas = cargarDatos();
-        const contenedorTareas = document.getElementById('tareas');
         contenedorTareas.innerHTML = '';
-
         tareas.forEach((tarea, indice) => {
             const tarjetaTarea = document.createElement('article');
             tarjetaTarea.classList.add('tarea-card');
             tarjetaTarea.innerHTML = `
             <h3>${tarea.titulo}</h3>
             <progress value="${tarea.progreso}" max="100">${tarea.progreso}</progress>
-            <button onclick="verDetalles('${tarea.tareaId}')">Ver detalles</button>
-        `;
+            <button onclick="abrirDialogo('${tarea.tareaId}')">Ver detalles</button>
+            <dialog id="${tarea.tareaId}">
+                    <h3>${tarea.titulo}</h3>
+                    <p id="descripcion-tarea">${tarea.descripcion}</p>
+                    <p>Fecha límite: <time id="fecha-limite">${tarea.fechaLimite}</time></p>
+                    <p>Prioridad: <meter id="prioridad-tarea" min="0" max="1">${tarea.prioridad}</meter></p>
+                    <button onclick="cerrarDialogo()">Cerrar</button>
+            </dialog>
+            `;
             contenedorTareas.appendChild(tarjetaTarea);
         });
+    }
+
+    //Función para mostrar el dialog con los detalles
+    abrirDialogo = (id) => {
+        dialog = document.getElementById(id);
+        dialog.showModal();
+    }
+
+    //Función para cerrar el dialog con los detalles
+    cerrarDialogo = () =>{
+        dialog.close();
     }
 
     // Recoger los datos del formulario
