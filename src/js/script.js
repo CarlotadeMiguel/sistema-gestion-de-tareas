1cc6ced48e49c76239000227d34e5d9269d55ba6
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-03-17 19:03:11 +0100

    feat: Crear formulario para las tareas y mostrarlas

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
-console.log('DOMContentLoaded');
+    console.log('DOMContentLoaded');
+});
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* Gestor de Tareas */
+
+    // Función para cargar datos desde localStorage
+    const cargarDatos = () => {
+        return JSON.parse(localStorage.getItem('tareas')) || [];
+    }
+
+    // Función para guardar datos en localStorage
+    const guardarDatos = (tareas) => {
+        localStorage.setItem('tareas', JSON.stringify(tareas));
+    }
+
+    // Función para agregar una nueva tarea
+    const agregarTarea = (tarea) => {
+        const tareas = cargarDatos();
+        tareas.push(tarea);
+        guardarDatos(tareas);
+    }
+
+    // Función para mostrar las tareas guardadas
+    const mostrarTareas = () => {
+        const tareas = cargarDatos();
+        const contenedorTareas = document.getElementById('tareas');
+        contenedorTareas.innerHTML = '';
+
+        tareas.forEach((tarea, indice) => {
+            const tarjetaTarea = document.createElement('article');
+            tarjetaTarea.classList.add('tarea-card');
+            tarjetaTarea.innerHTML = `
+            <h3>${tarea.titulo}</h3>
+            <progress value="${tarea.progreso}" max="100">${tarea.progreso}</progress>
+            <button onclick="verDetalles('${tarea.tareaId}')">Ver detalles</button>
+        `;
+            contenedorTareas.appendChild(tarjetaTarea);
+        });
+    }
+
+    // Recoger los datos del formulario
+    document.getElementById('formularioTareas').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const titulo = document.getElementById('titulo').value;
+        const tareaId = titulo.trim();
+        const descripcion = document.getElementById('descripcion').value;
+        const fechaLimite = document.getElementById('fechaLimite').value;
+        const prioridad = document.getElementById('prioridad').value;
+        const progreso = document.getElementById('progreso').value ? document.getElementById('progreso').value : 0;
+        const dependencias = document.getElementById('dependencias').value;
+
+        const nuevaTarea = {
+            titulo,
+            tareaId,
+            descripcion,
+            fechaLimite,
+            prioridad,
+            progreso,
+            dependencias
+        };
+
+        agregarTarea(nuevaTarea);
+
+        // Limpiar el formulario
+        document.getElementById('formularioTareas').reset();
+
+        // Mostrar las tareas actualizadas
+        mostrarTareas();
+    });
+
+    // Mostrar las tareas al cargar la página
+    mostrarTareas();
+
 });
