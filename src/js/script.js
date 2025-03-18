5d1f14d84916b55940d78a771f45db73636091fd
karlo1989nu <karlo.navarro@tajamar3695.com>
2025-03-18 16:42:14 +0100

    subiendo las constantes globales al inicio del js

--- src/js/script.js
+++ src/js/script.js
@@ -1,110 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Constantes globales
+let dialog;
+const form = document.getElementById("form-reserva");
+const telefonoInput = document.getElementById("telefono");
+const fechaInput = document.getElementById("fecha");
+const errorMessage = document.getElementById("error-message");
+const reservasContainer = document.createElement("div");
+reservasContainer.id = "reservas-container";
+document.body.appendChild(reservasContainer);
 
-    /* Gestor de Tareas */
-    const contenedorTareas = document.getElementById('tareas');
-    let dialog;
-
-    // Función para cargar datos desde localStorage
-    const cargarDatos = () => {
-        return JSON.parse(localStorage.getItem('tareas')) || [];
-    }
-    let tareas = cargarDatos();
-
-    // Función para guardar datos en localStorage
-    const guardarDatos = (tareas) => {
-        localStorage.setItem('tareas', JSON.stringify(tareas));
-    }
-
-    // Función para agregar una nueva tarea
-    const agregarTarea = (tarea) => {
-        tareas.push(tarea);
-        guardarDatos(tareas);
-    }
-
-    //Función para eliminar una tarea
-    eliminarTarea = (id) => {
-        tareas = tareas.filter(tarea => tarea.tareaId !== id);
-        guardarDatos(tareas);
-        mostrarTareas();
-    }
-
-    // Función para mostrar las tareas guardadas
-    const mostrarTareas = () => {
-        contenedorTareas.innerHTML = '';
-        tareas.forEach((tarea) => {
-            const tarjetaTarea = document.createElement('article');
-            tarjetaTarea.classList.add('tarea-card');
-            tarjetaTarea.innerHTML = `
-            <h3>${tarea.titulo}</h3>
-            <progress value="${tarea.progreso}" max="100">${tarea.progreso}</progress>
-            <button onclick="abrirDialogo('${tarea.tareaId}')">Ver detalles</button>
-            <button onclick="eliminarTarea('${tarea.tareaId}')">Borrar Tarea</button>
-            <dialog id="${tarea.tareaId}">
-                    <h3>${tarea.titulo}</h3>
-                    <p id="descripcion-tarea">${tarea.descripcion}</p>
-                    <p>Fecha límite: <time id="fecha-limite">${tarea.fechaLimite}</time></p>
-                    <p>Prioridad: <meter id="prioridad-tarea" min="0" max="1">${tarea.prioridad}</meter></p>
-                    <button onclick="cerrarDialogo()">Cerrar</button>
-            </dialog>
-            `;
-            contenedorTareas.appendChild(tarjetaTarea);
-        });
-    }
-
-    //Función para mostrar el dialog con los detalles
-    abrirDialogo = (id) => {
-        dialog = document.getElementById(id);
-        dialog.showModal();
-    }
-
-    //Función para cerrar el dialog con los detalles
-    cerrarDialogo = () => {
-        dialog.close();
-    }
-
-    // Recoger los datos del formulario
-    document.getElementById('formularioTareas').addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const titulo = document.getElementById('titulo').value;
-        const numId = tareas.length + 1;
-        const tareaId = titulo.trim() + numId;
-        const descripcion = document.getElementById('descripcion').value;
-        const fechaLimite = document.getElementById('fechaLimite').value;
-        const prioridad = document.getElementById('prioridad').value;
-        const progreso = document.getElementById('progreso').value ? document.getElementById('progreso').value : 0;
-        const dependencias = document.getElementById('dependencias').value;
-
-        const nuevaTarea = {
-            titulo,
-            tareaId,
-            descripcion,
-            fechaLimite,
-            prioridad,
-            progreso,
-            dependencias
-        };
-
-        agregarTarea(nuevaTarea);
-
-        // Limpiar el formulario
-        document.getElementById('formularioTareas').reset();
-
-        // Mostrar las tareas actualizadas
-        mostrarTareas();
-    });
-
-    // Mostrar las tareas al cargar la página
-    mostrarTareas();
+document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
-
-  const form = document.getElementById("form-reserva");
-  const telefonoInput = document.getElementById("telefono");
-  const fechaInput = document.getElementById("fecha");
-  const errorMessage = document.getElementById("error-message");
-  const reservasContainer = document.createElement("div");
-  reservasContainer.id = "reservas-container";
-  document.body.appendChild(reservasContainer);
 
   // Validación de fecha para no permitir días pasados
   const today = new Date().toISOString().split("T")[0];
@@ -141,8 +46,6 @@ document.addEventListener('DOMContentLoaded', () => {
       const reserva = {
         id: Date.now(),
         cliente: form.nombre.value,
-        cliente2: form.apellido1.value,
-        cliente3: form.apellido2.value,
         telefono: form.telefono.value,
         fecha: form.fecha.value,
         personas: form.personas.value,
@@ -159,8 +62,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Función para renderizar las reservas
-
   function renderReservas() {
     const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
     const fragment = document.createDocumentFragment();
@@ -169,13 +70,13 @@ document.addEventListener('DOMContentLoaded', () => {
       const card = document.createElement("div");
       card.classList.add("reserva-card");
       card.innerHTML = `
-            <h3>Reserva #${reserva.id}</h3>
-            <p>Cliente: ${reserva.cliente} ${reserva.cliente2} ${reserva.cliente3}</p>
-            <p>Teléfono: ${reserva.telefono}</p>
-            <p>Fecha: ${reserva.fecha}</p>
-            <p>Personas: ${reserva.personas}</p>
-            <p>Estado: ${reserva.estado}</p>
-        `;
+                <h3>Reserva #${reserva.id}</h3>
+                <p>Cliente: ${reserva.cliente}</p>
+                <p>Teléfono: ${reserva.telefono}</p>
+                <p>Fecha: ${reserva.fecha}</p>
+                <p>Personas: ${reserva.personas}</p>
+                <p>Estado: ${reserva.estado}</p>
+            `;
       fragment.appendChild(card);
     });
 
