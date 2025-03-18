1f699887e6cf9d549c6c1b13a888430f3fddd856
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-03-18 17:05:32 +0100

    poner fecha minima a la fecha limite para que no se pueda poner una fecha en pasado

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    /* Gestor de Tareas */
-    const contenedorTareas = document.getElementById('tareas');
-    let dialog;
+  /* Gestor de Tareas */
+  const contenedorTareas = document.getElementById('tareas');
+  let dialog;
 
-    // Función para cargar datos desde localStorage
-    const cargarDatos = () => {
-        return JSON.parse(localStorage.getItem('tareas')) || [];
-    }
-    let tareas = cargarDatos();
+  const fechaInputMin = document.getElementById('fechaLimite');
+  
+  // Obtener la fecha actual
+  const hoy = new Date();
+  const dia = hoy.getDate().toString().padStart(2, '0');
+  const mes = (hoy.getMonth() + 1).toString().padStart(2, '0');
+  const anio = hoy.getFullYear();
 
-    // Función para guardar datos en localStorage
-    const guardarDatos = (tareas) => {
-        localStorage.setItem('tareas', JSON.stringify(tareas));
-    }
+  // Formatear la fecha como YYYY-MM-DD
+  const fechaMinima = `${anio}-${mes}-${dia}`;
 
-    // Función para agregar una nueva tarea
-    const agregarTarea = (tarea) => {
-        tareas.push(tarea);
-        guardarDatos(tareas);
-    }
+  // Asignar la fecha mínima al campo date
+  fechaInputMin.setAttribute('min', fechaMinima);
 
-    //Función para eliminar una tarea
-    eliminarTarea = (id) => {
-        tareas = tareas.filter(tarea => tarea.tareaId !== id);
-        guardarDatos(tareas);
-        mostrarTareas();
-    }
+  // Función para cargar datos desde localStorage
+  const cargarDatos = () => {
+    return JSON.parse(localStorage.getItem('tareas')) || [];
+  }
+  let tareas = cargarDatos();
 
-    // Función para mostrar las tareas guardadas
-    const mostrarTareas = () => {
-        contenedorTareas.innerHTML = '';
-        tareas.forEach((tarea) => {
-            const tarjetaTarea = document.createElement('article');
-            tarjetaTarea.classList.add('tarea-card');
-            tarjetaTarea.innerHTML = `
+  // Función para guardar datos en localStorage
+  const guardarDatos = (tareas) => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }
+
+  // Función para agregar una nueva tarea
+  const agregarTarea = (tarea) => {
+    tareas.push(tarea);
+    guardarDatos(tareas);
+  }
+
+  //Función para eliminar una tarea
+  eliminarTarea = (id) => {
+    tareas = tareas.filter(tarea => tarea.tareaId !== id);
+    guardarDatos(tareas);
+    mostrarTareas();
+  }
+
+  // Función para mostrar las tareas guardadas
+  const mostrarTareas = () => {
+    contenedorTareas.innerHTML = '';
+    tareas.forEach((tarea) => {
+      const tarjetaTarea = document.createElement('article');
+      tarjetaTarea.classList.add('tarea-card');
+      tarjetaTarea.innerHTML = `
             <h3>${tarea.titulo}</h3>
             <progress value="${tarea.progreso}" max="100">${tarea.progreso}</progress>
             <button onclick="abrirDialogo('${tarea.tareaId}')">Ver detalles</button>
@@ -47,55 +61,55 @@ document.addEventListener('DOMContentLoaded', () => {
                     <button onclick="cerrarDialogo()">Cerrar</button>
             </dialog>
             `;
-            contenedorTareas.appendChild(tarjetaTarea);
-        });
-    }
-
-    //Función para mostrar el dialog con los detalles
-    abrirDialogo = (id) => {
-        dialog = document.getElementById(id);
-        dialog.showModal();
-    }
-
-    //Función para cerrar el dialog con los detalles
-    cerrarDialogo = () => {
-        dialog.close();
-    }
-
-    // Recoger los datos del formulario
-    document.getElementById('formularioTareas').addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const titulo = document.getElementById('titulo').value;
-        const numId = tareas.length + 1;
-        const tareaId = titulo.trim() + numId;
-        const descripcion = document.getElementById('descripcion').value;
-        const fechaLimite = document.getElementById('fechaLimite').value;
-        const prioridad = document.getElementById('prioridad').value;
-        const progreso = document.getElementById('progreso').value ? document.getElementById('progreso').value : 0;
-        const dependencias = document.getElementById('dependencias').value;
-
-        const nuevaTarea = {
-            titulo,
-            tareaId,
-            descripcion,
-            fechaLimite,
-            prioridad,
-            progreso,
-            dependencias
-        };
-
-        agregarTarea(nuevaTarea);
-
-        // Limpiar el formulario
-        document.getElementById('formularioTareas').reset();
-
-        // Mostrar las tareas actualizadas
-        mostrarTareas();
+      contenedorTareas.appendChild(tarjetaTarea);
     });
+  }
 
-    // Mostrar las tareas al cargar la página
+  //Función para mostrar el dialog con los detalles
+  abrirDialogo = (id) => {
+    dialog = document.getElementById(id);
+    dialog.showModal();
+  }
+
+  //Función para cerrar el dialog con los detalles
+  cerrarDialogo = () => {
+    dialog.close();
+  }
+
+  // Recoger los datos del formulario
+  document.getElementById('formularioTareas').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const titulo = document.getElementById('titulo').value;
+    const numId = tareas.length + 1;
+    const tareaId = titulo.trim() + numId;
+    const descripcion = document.getElementById('descripcion').value;
+    const fechaLimite = document.getElementById('fechaLimite').value;
+    const prioridad = document.getElementById('prioridad').value;
+    const progreso = document.getElementById('progreso').value ? document.getElementById('progreso').value : 0;
+    const dependencias = document.getElementById('dependencias').value;
+
+    const nuevaTarea = {
+      titulo,
+      tareaId,
+      descripcion,
+      fechaLimite,
+      prioridad,
+      progreso,
+      dependencias
+    };
+
+    agregarTarea(nuevaTarea);
+
+    // Limpiar el formulario
+    document.getElementById('formularioTareas').reset();
+
+    // Mostrar las tareas actualizadas
     mostrarTareas();
+  });
+
+  // Mostrar las tareas al cargar la página
+  mostrarTareas();
   console.log("DOMContentLoaded");
 
   const form = document.getElementById("form-reserva");
